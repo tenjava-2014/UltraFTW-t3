26034907f40dc46702d56811b950e92996fe8afd
Ultra <Ultra.Rocks.XD@gmail.com>
2014-07-12 11:06:17 -0400

    Added some random actions, and added a way to pick an action. The puzzle pieces are slowly coming together...
    I really wish I would've did some practice with writing Java yesterday. I'm too used to web programming. :P

---
+++ src/main/java/com/tenjava/entries/UltraFTW/t3/Action.java
@@ -0,0 +1,29 @@
+package com.tenjava.entries.UltraFTW.t3;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Represents a random event/action that can happen to a player.
+ */
+public enum Action
+{
+    /**
+     * A simple lightning strike. Or two.
+     */
+    LIGHTNING,
+    /**
+     * Gravity for blocks? In Minecraft? WHAT?
+     */
+    GRAVITY,
+    /**
+     * Don't look back...
+     */
+    CREEP_SCARE,
+    /**
+     * This/these diamond(s) may look pretty smokin', but they're not the only thing that's hot.
+     */
+    HOT_DIAMONDS
+}
--- src/main/java/com/tenjava/entries/UltraFTW/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/UltraFTW/t3/TenJava.java
@@ -7,9 +7,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class TenJava extends JavaPlugin implements Listener
 {
@@ -17,7 +15,7 @@ public class TenJava extends JavaPlugin implements Listener
     private String startMessage = "UltraFTW's 2014 ten.java plugin was ";
 
     // current online players (NOT CONTINUALLY UPDATED; THIS UPDATED WHEN IT PLEASES)
-    public List players = new ArrayList();
+    public List<UUID> players = new ArrayList<UUID>();
 
     // amount of game ticks per second, for scheduler
     int ticksPerSec = 20;
@@ -43,6 +41,7 @@ public class TenJava extends JavaPlugin implements Listener
         players.add(event.getPlayer().getUniqueId());
         JavaPlugin plugin = this;
 
+        // will
         new BukkitRunnable()
         {
             @Override
@@ -64,5 +63,22 @@ public class TenJava extends JavaPlugin implements Listener
         int max = 25*ticksPerMin;
 
         return new Random().nextInt(max - min + 1) + min;
+    }
+
+    public UUID pickPlayer()
+    {
+        /*for (UUID player : players)
+        {
+
+        }*/
+
+        return null; // only temporarily so I can commit/push this
+    }
+
+    public static Action randomAction()
+    {
+        List<Action> actions = Collections.unmodifiableList(Arrays.asList(com.tenjava.entries.UltraFTW.t3.Action.values()));
+        Random rand = new Random();
+        return actions.get(rand.nextInt(actions.size()));
     }
 }
